493936387513a59383fdb010099a9057442724e6
yqm-307 <979336542@qq.com>
2024-05-14 16:11:04 +0800

    bbtbase修改，同步兼容errcode。

--- bbt/http/detail/Errcode.hpp
+++ bbt/http/detail/Errcode.hpp
@@ -25,7 +25,9 @@ public:
     explicit Errcode(const std::string& msg, emErr err_type = emErr::Failed):TBase(msg, err_type) {}
     virtual ~Errcode() {}
 
-    const std::string& What() { return GetMsg(); }
+    virtual const std::string&  What() const override  { return GetMsg(); }
+    virtual const char*         CWhat() const override { return What().c_str(); }
+    virtual const emErr&        Type() const override  { return GetErrType(); }
 };
 
 } // namespace bbt::http
--- shell/install.sh
+++ shell/install.sh
@@ -1,35 +1,14 @@
 
 installpath="/usr/local/include"
-
-function generate_head_dir_and_copy_to_dstdir()
-{
-    dstdir=$1
-    cp -rf bbt/http src_h
-    find src_h -name '*.c' -o -name '*.cc' -print | xargs rm
-
-    if [ ! -d "$dstdir/bbt" ];then
-        sudo mkdir $dstdir/bbt
-    fi
-
-    if [ -d "$dstdir/bbt/http" ]
-    then
-        sudo rm -rf $dstdir/bbt/http
-    fi
-
-    sudo mv src_h $dstdir/bbt/http
-    # info_build "copy over! cpp head file copy to ${dstdir}/bbt"
-    rm -rf src_h
-}
+libpath="/usr/local/lib"
 
 cd ..
-generate_head_dir_and_copy_to_dstdir $installpath
+shell/bbt_copy_header_dir.sh $installpath bbt http
+
+sudo cp build/lib/libybbt_http.so /usr/local/lib/
 
 if [ ! -d "build" ];then
     mkdir build
 fi
 
-cd build
-cmake ..
-make -j4
-sudo cp -rf lib/libybbt_http.so /usr/local/lib/
 echo "安装完成"
